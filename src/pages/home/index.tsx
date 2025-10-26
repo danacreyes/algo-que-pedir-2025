7bdf38af370d5c6fcbd95320e02fc20234adc9dc
Fernanda Perez <fernanda.tperez@outlook.com>
2025-10-26 00:50:52 -0300

    comienzo del navbar, falta ver en el ruteo como hacer para que quede abajo

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Button, Container } from '@mui/material'
+import * as React from 'react'
+
+
+export const Home: React.FC<{}> = () => {
+  return (
+    <Container>
+      <Button>
+        pagina home
+      </Button>
+    </Container>
+  )
+}
---
+++ src/routes/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from '../pages/home'
+import { RouterLayout } from '../common/routerLayout'
+
+export const AppRouter: React.FC<{}> = () => {
+    return (
+        <Routes>
+            <Route  path="/" element={<RouterLayout/>}>
+                <Route  path="/" element={<Home/>}/>
+            </Route>
+        </Routes>
+    )
+}
