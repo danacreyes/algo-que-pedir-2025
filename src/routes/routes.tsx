4003a0258d789e6be72677888f895bf360a98156
CatalinaCorrea <catalinayazmincorrea@gmail.com>
2025-11-01 15:02:48 -0300

    AuthProvider para Routing protegido

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,23 +1,31 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Login from '../pages/auth/Login'
-import OrderDetails from '../pages/OrderDetails'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { RouterLayout } from './RouterLayout'
+import RequireAuth from './auth/RequireAuth'
+import Login from '../pages/auth/Login'
+import Register from '../pages/auth/Register'
+import Home from '../pages/Home'
+import OrderDetails from '../pages/OrderDetails'
 import Profile from '../pages/Profile'
 import SearchCriteria from '../pages/SearchCriteria'
-import Register from '../pages/auth/Register'
 
 export const AppRouter = () => {
     return (
         <Routes>
-            <Route  path="/" element={<RouterLayout/>}>
-                {/* <Route  path="/tu-ruta" element={<TuPagina/>}/> */}
+            <Route path="/" element={<RouterLayout/>}>
+                {/* públicas */}
                 <Route  path="/login" element={<Login />}/>
                 <Route  path="/register" element={<Register />}/>
-                <Route  path="/" element={<Home/>}/>
-                <Route  path="/order-details" element={<OrderDetails/>}/>
-                <Route  path="/profile" element={<Profile/>}/>
-                <Route  path="/search-criteria" element={<SearchCriteria/>}/>
+                
+                {/* protegidas */}
+                <Route element={<RequireAuth/>}>
+                    <Route index element={<Home/>}/>
+                    <Route  path="/order-details" element={<OrderDetails/>}/>
+                    <Route  path="/profile" element={<Profile/>}/>
+                    <Route  path="/search-criteria" element={<SearchCriteria/>}/>
+                </Route>
+                
+                {/* fallback */}
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Route>
         </Routes>
     )
