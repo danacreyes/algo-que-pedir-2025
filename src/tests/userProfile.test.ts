d7f352bc5c6e158fae231cecc9b6ccfde1fa3ad1
dcreyes <dcossettinireyes@estudiantes.unsam.edu.ar>
2025-11-22 14:14:43 -0300

    generacion de rama para test de profile

---
+++ src/tests/userProfile.test.ts
@@ -0,0 +1,7 @@
+import { describe, test } from 'vitest'
+
+describe('Tests para UserProfile', () => {
+  test('Agregar un ingrediente a evitar a un ingrediente preferido genera error', () => {
+    
+  })
+})
